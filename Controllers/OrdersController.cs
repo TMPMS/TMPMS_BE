c301e217f05d362fe99108fd8be204f8d2609889
Angel D Hit <phatdang21082004@gmail.com>
2026-08-06 02:24:47 +0700

    fix api/

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -8,7 +8,7 @@ using TMPMS.Data;
 namespace TMPMS.Controllers
 {
     [ApiController]
-    [Route("api")]
+    [Route("api/")]
     [Authorize]
     public class OrdersController : ControllerBase
     {
